76dece4321097e0a825330a4ace02b8624cc3f09
taogoat <the.taogaot@gmail.com>
2017-04-21 15:21:17 -0700

    auto built commit

--- src/core/client/index.js
+++ src/core/client/index.js
@@ -1,7 +1,41 @@
-import Vue from 'vue'
-import App from './App.vue'
+var start = null
+var unstart = null
+function spin(timestamp){
+  var duration = 6000
+    if (start === null) {
+        start = timestamp;
+    }
+  var runtime = Number(timestamp) - Number(start)
+  var progress = runtime / duration
+  progress = Math.min(progress, 1)
+  var xy = 32-(32*progress)
+  var rotated = (1080*progress)
+  var trans = 'translate('+xy+','+xy+')scale('+progress+')rotate('+rotated+',32,32)'
+  var targ = document.getElementById('taijitu')
+  targ.setAttribute("transform", trans)
+    if (progress < 1) {
+        requestAnimationFrame(spin)
+    }
+    else{unstart = null; requestAnimationFrame(unspin)}
+}
 
-new Vue({
-  el: '#root',
-  render: h => h(App)
-})
+function unspin(timestamp){
+  var duration = 6000
+    if (unstart === null) {
+        unstart = timestamp;
+    }
+  var runtime = Number(timestamp) - Number(unstart)
+  var progress = runtime / duration
+  progress = Math.min(progress, 1)
+  var xy = 32*progress
+  var rotated = (1080*progress)
+  var scaled = Number(1-progress)
+  var trans = 'translate('+xy+','+xy+')scale('+scaled+')rotate('+rotated+',32,32)'
+  var targ = document.getElementById('taijitu')
+  targ.setAttribute("transform", trans)
+    if (progress < 1) {
+        requestAnimationFrame(unspin)
+    }
+    else{start = null; requestAnimationFrame(spin)}
+}
+window.requestAnimationFrame(spin)
